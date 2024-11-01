2f8c600dff2940f42fbf564a0958ac9dcf4c3cf5
waterricecake <fruturum@nate.com>
2024-11-01 20:53:03 +0900

    feat : set bi dependency each package

--- src/main/java/org/example/dependencystudy/a/A.java
+++ src/main/java/org/example/dependencystudy/a/A.java
@@ -1,4 +1,7 @@
 package org.example.dependencystudy.a;
 
+import org.example.dependencystudy.b.B;
+
 public class A {
+    private B b;
 }
--- src/main/java/org/example/dependencystudy/b/B.java
+++ src/main/java/org/example/dependencystudy/b/B.java
@@ -1,4 +1,7 @@
 package org.example.dependencystudy.b;
 
+import org.example.dependencystudy.a.A;
+
 public class B {
+    private A a;
 }
